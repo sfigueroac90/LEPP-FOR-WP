06a03c53127ece16dbf6898e4c00c88365f8a076
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-11 08:38:07 -0500

    [EDIT] Special commentaries about build react in homepage

--- create_test_reactapp.sh
+++ create_test_reactapp.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-#https://www.linode.com/docs/development/nodejs/how-to-install-nodejs-and-nginx-on-centos-8/co
+#https://www.linode.com/docs/development/nodejs/how-to-install-nodejs-and-nginx-on-centos-8/
+#https://medium.com/@timmykko/deploying-create-react-app-with-nginx-and-ubuntu-e6fe83c5e9e7
+#https://medium.com/@jgefroh/a-guide-to-using-nginx-for-static-websites-d96a9d034940
 echo "<¬¬¬¬¬>CREATING TESTING REACT APP<¬¬¬¬¬>"
 mkdir /home/vagrant/documents
 cd /home/vagrant/documents
 npx create-react-app testapp
 cd testapp
 yarn add serve
+#https://return2.net/deploy-react-app-build-to-any-subdirectory/
+#https://stackoverflow.com/questions/43011207/using-homepage-in-package-json-without-messing-up-paths-for-localhost
 yarn build 
 # copying folder
 cp -r build/ /var/www/    
