2e2a1cb34eb2a60259510742efb3585c775085f8
Sebastián Figueroa <sebastian.figueroa@parqueexplora.org>
2020-09-12 09:59:53 -0500

    Fixes tu run automatically NGINX PHP-FPM

--- startup_php.sh
+++ startup_php.sh
@@ -10,9 +10,13 @@ sudo dnf module enable php:remi-7.4 -y
 sudo dnf install php php-cli php-common -y
 #sudo dnf install -y php-mysql
 sudo yum install php-pgsql -y
-sudo sed -i 's/apache/nginx/g' /etc/php-fpm.d/www.conf
+#sudo sed -i 's/apache/nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/user = apache/user = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/group = apache/group = nginx/g' /etc/php-fpm.d/www.conf
+
+
 #Create php test
-sudo chown -R ${USER}.${USER} /usr/share/nginx/html/
+#sudo chown -R ${USER}.${USER} /usr/share/nginx/html/
 echo '<?php
 
 phpinfo(); 
@@ -24,6 +28,7 @@ sudo systemctl enable php-fpm
 #vagrasudo systemctl restart httpd
 #Verifing php-fpm is running
 sudo systemctl status php-fpm.service
+sudo systemctl restart nginx
 php -v
 
 # sudo nano /etc/nginx/conf.d/default.conf
